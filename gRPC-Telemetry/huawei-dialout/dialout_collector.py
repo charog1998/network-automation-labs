4b00bd0b2d04b6a4a6855dcf5b09421135ea5692
18746001388 <18746001388@139.com>
2026-01-05 12:29:42 +0800

    优化代码

--- gRPC-Telemetry/huawei-dialout/dialout_collector.py
+++ gRPC-Telemetry/huawei-dialout/dialout_collector.py
@@ -9,11 +9,8 @@ _ONE_DAY_IN_SECONDS = 60*60*24
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-
-    huawei_grpc_dialout_pb2_grpc.add_gRPCDataserviceServicer_to_server(Telemetry_CPU_Info(),server)
-
+    huawei_grpc_dialout_pb2_grpc.add_gRPCDataserviceServicer_to_server(TelemetryDataService(),server)
     server.add_insecure_port('192.168.1.5:20000')
-
     server.start()
 
     try:
@@ -22,20 +19,27 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
 
-class Telemetry_CPU_Info(huawei_grpc_dialout_pb2_grpc.gRPCDataserviceServicer):
+class TelemetryDataService(huawei_grpc_dialout_pb2_grpc.gRPCDataserviceServicer):
+    """
+    一个通用的Telemetry数据处理服务。
+    通过解析 'proto_path' 动态加载对应的 Protobuf 模块来解码数据。
+    """
     def __init__(self):
         return
     
     def dataPublish(self, request_iterator, context):
+        """
+        处理来自 gRPC 客户端的流式数据。
+        """
         for i in request_iterator:
-            print('################ start ##################\n')
+            print('############ New Telemetry Packet ############\n')
             telemetry_data = huawei_telemetry_pb2.Telemetry.FromString(i.data)
             print(telemetry_data)
 
             for row_data in telemetry_data.data_gpb.row:
-                print("------")
+                print("------------------ Proto is ------------------")
                 print(telemetry_data.proto_path)
-                print("------")
+                print("----------------------------------------------")
                 module_name = telemetry_data.proto_path.split(".")[0]
                 root_class = telemetry_data.proto_path.split(".")[1]
 
@@ -44,9 +48,9 @@ class Telemetry_CPU_Info(huawei_grpc_dialout_pb2_grpc.gRPCDataserviceServicer):
 
                 decode_func = getattr(decode_module,root_class).FromString
 
-                print("--------- content is ---------\n")
+                print("----------------- Content is -----------------\n")
                 print(decode_func(row_data.content))
-                print("------------ done ------------")
+                print("----------- Packet Processing Done -----------")
 
 if __name__ == "__main__":
     serve()
